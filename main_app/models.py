19eb4f8de2a154261cf61118eb0801e0b262d899
rojinapradhan19 <rojina.pradn.rp@gmail.com>
2020-07-24 16:33:23 +0545

    commit msg

--- main_app/models.py
+++ main_app/models.py
@@ -11,20 +11,25 @@ class Supplier(models.Model):
         return self.sup_name
 
 class Category(models.Model):
-    cat_name = models.TextField(max_length=200)
+    cat_name = models.TextField(max_length=200,blank = True, null = True)
 
     def __str__(self):
         return self.cat_name
 
 class Sub_Category(models.Model):
-    category = models.ForeignKey(Category, on_delete = models.CASCADE)
-    subCat_name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete = models.CASCADE,blank = True, null = True)
+    subCat_name = models.CharField(max_length=100,blank = True, null = True)
 
     def __str__(self):
         return self.subCat_name
 
 class Brand(models.Model):
-    brand_name = models.CharField(max_length=50)
+    BRANDNAME = ( 
+        ('Dabar', 'Dabar'),
+        ('Parle', 'Parle'),
+        ('Britaniya', 'Britaniya'),
+    )
+    brand_name = models.CharField(max_length=50, choices = BRANDNAME,blank = True, null = True)
 
     def __str__(self):
         return self.brand_name
@@ -33,9 +38,9 @@ class Item(models.Model):
     item_name = models.CharField(max_length=50)
     item_price = models.DecimalField(max_digits=8, decimal_places=2)
     item_quantity = models.DecimalField(max_digits=5, decimal_places=2)
-    brand = models.ForeignKey(Brand, on_delete= models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    subCategory = models.ForeignKey(Sub_Category, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete= models.CASCADE, blank = True, null = True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE , blank = True , null = True)
+    subCategory = models.ForeignKey(Sub_Category, on_delete=models.CASCADE , blank = True, null= True)
     supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -43,10 +48,10 @@ class Item(models.Model):
 
 class Order(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
-    no_of_product = models.DecimalField(max_digits=5)
+    no_of_product = models.DecimalField(max_digits=5,  decimal_places=1)
     amt = models.FloatField(max_length=8)
 
-    def_str_(self):
-        return item
+    def _str_(self):
+        return self.item
 
 
